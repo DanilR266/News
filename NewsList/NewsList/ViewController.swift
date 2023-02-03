8e2af8be07d807c2b33998c9d5e96cddb07c03b8
DanilR266 <danilwind8@gmail.com>
2023-02-03 16:25:58 +0300

    initial commit

---
+++ NewsList/NewsList/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NewsList
+//
+//  Created by Данила on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
