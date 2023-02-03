af9848f181fb08231c2d2c78d9395483cfce0a9b
DanilR266 <danilwind8@gmail.com>
2023-02-03 17:03:10 +0300

    first commit

---
+++ NewsList/NewsList/Cell.swift
@@ -0,0 +1,15 @@
+//
+//  Cell.swift
+//  NewsList
+//
+//  Created by Данила on 03.02.2023.
+//
+
+import Foundation
+import UIKit
+
+struct CellItems {
+    var label: UILabel
+    var image: UIImage
+    var count: UILabel
+}
--- NewsList/NewsList/ViewController.swift
+++ NewsList/NewsList/ViewController.swift
@@ -7,14 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
-    
+    let cellId = "cellid"
+    var cellItems: [CellItems] = [CellItems]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
     }
+    override func didReceiveMemoryWarning() {
+     super.didReceiveMemoryWarning()
+     // Dispose of any resources that can be recreated.
+     }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let currentLastItem = cellItems[indexPath.row]
+        cell.textLabel?.text = currentLastItem.label.text!
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellItems.count
+     }
+    
+    func createProductArray() {
+        cellItems.append(CellItems(label: UILabel(), image: <#T##UIImage#>, count: <#T##UILabel#>))
+        }
 
 
 }
